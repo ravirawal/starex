5db2f94909e7ff3bc61821fdcb533ba01dd53591
ravirawal <ravirawal33@gmail.com>
2025-07-10 15:59:59 +0530

    footer changes

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { FlexLayoutModule } from 'ngx-flexible-layout';
+import { MatIconModule } from "@angular/material/icon";
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FlexLayoutModule, MatIconModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
