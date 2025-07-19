f958daff8c37322cd01c914acad5d4b150668652
ravirawal <ravirawal33@gmail.com>
2025-07-19 10:07:57 +0530

    navigation setup desktop

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -6,10 +6,14 @@ import { Router } from '@angular/router';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {RouterLink} from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule,
+  imports: [MatToolbarModule, MatListModule, CommonModule,MatCardModule,MatTabsModule,
     MatButtonModule,RouterLink,
     MatIconModule, MatMenuModule, MatExpansionModule],
   templateUrl: './header.html',
@@ -17,9 +21,31 @@ import {RouterLink} from '@angular/router';
 })
 export class Header {
    @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+   screenWidth = signal(window.innerWidth);
    constructor(private router: Router){
-
+window.addEventListener('resize', () => {
+      this.screenWidth.set(window.innerWidth);
+    });
   }
+  isMobile(): boolean {
+    return this.screenWidth() < 768;
+  }
+  navItems = [
+    {
+      label: 'Dashboard',
+      children: [
+        { label: 'Overview', route: '/dashboard/overview' },
+        { label: 'Stats', route: '/dashboard/stats' },
+      ],
+    },
+    {
+      label: 'Settings',
+      children: [
+        { label: 'Profile', route: '/settings/profile' },
+        { label: 'Security', route: '/settings/security' },
+      ],
+    },
+  ]
   readonly panelOpenState = signal(false);
   handleDivClick(event: MouseEvent): void {
   event.stopPropagation(); // prevents bubbling that triggers close
