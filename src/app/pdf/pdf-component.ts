434c6e799d6d3de0a51cbcd4fe354fa08f7a0eb4
ravirawal <ravirawal33@gmail.com>
2025-09-01 22:21:23 +0530

    check for build pass

--- src/app/pdf/pdf-component.ts
+++ src/app/pdf/pdf-component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { PdfService } from './pdf-component-service';
 import * as pdfjsLib from 'pdfjs-dist';
-import workerUrl from 'pdfjs-dist/build/pdf.worker?url';
-import { GlobalWorkerOptions } from 'pdfjs-dist';
+//import workerUrl from 'pdfjs-dist/build/pdf.worker?url';
+//import { GlobalWorkerOptions } from 'pdfjs-dist';
 import { MatIconModule } from "@angular/material/icon";
+import { GlobalWorkerOptions } from 'pdfjs-dist';
 
-GlobalWorkerOptions.workerSrc = workerUrl;
+// Use import.meta.url to resolve the worker path
+GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.mjs',
+  import.meta.url
+).toString();
+
+//GlobalWorkerOptions.workerSrc = workerUrl;
 
 @Component({
     selector: 'app-pdf-viewer',
